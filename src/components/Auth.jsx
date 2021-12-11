9f6adf37b2cdefa7908c3d2f8a9a83623dc5180c
unknown <aru.krishnan2015@gmail.com>
2021-12-11 15:56:41 +0530

    final

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -27,7 +27,7 @@ const Auth = () => {
 
         const { username, password, phoneNumber, avatarURL } = form;
 
-        const URL = 'https://medical-pager.herokuapp.com/auth';
+        const URL = 'http://medical-pager.herokuapp.com/auth';
 
         
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
